0fdd13a98a120ab81f83d25ee26d49edfcc0dfa3
martinmalinda <malindacz@gmail.com>
2017-02-10 16:01:13 +0100

    run composer install in willBuild

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 /* jshint node: true */
 'use strict';
 
+var BasePlugin = require('ember-cli-deploy-plugin');
+var sys = require('sys')
+var exec = require('child_process').exec;
+var RSVP = require('rsvp');
+
 module.exports = {
   name: 'ember-cli-deploy-composer'
+
+  createDeployPlugin: function(options) {
+    var DeployPlugin = BasePlugin.extend({
+      name: options.name,
+
+      willBuild: function(context) {
+        var _this = this;
+        return new RSVP.Promise(function(resolve, reject){
+          _this.log('executing composer install');
+          exec('composer install', function(error, stdout, stderr){
+            if(error){
+              _this.log('composer install errored');
+              reject(error);
+            }
+
+            if(stderr){
+              _this.log('composer install errored');
+              reject(stderr);
+            }
+
+            _this.log('composer install was successful');
+            resolve(stdout);
+          });
+        });
+      },
+    });
+
+    return new DeployPlugin();
+  }
 };
